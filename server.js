f5d438f10418695dbf4cc1d6f9b016f1d4eb3a78
Asim Shah <asimshah2111@gmail.com>
2024-03-11 00:53:02 +0530

    added scoping

--- server.js
+++ server.js
@@ -1,31 +1,48 @@
-import OpenAI from "openai";
+import OpenAI from "openai"
 import dotenv from "dotenv"
-dotenv.config()
 import express from "express"
 import bodyParser from "body-parser"
+
+dotenv.config()
 const app = express()
-const PORT = 3000;
+const PORT = 3000
 
-
-const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
 app.use(bodyParser.json())
 
-app.post('/chat', async(req,res)=>{
-  
- const {prompt} = req.body;
+async function findScope(question) {
+    const result = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "user",
+                content: `Is the question related to geography? Answer with yes or no only: ${question}`,
+            },
+        ],
+        model: "gpt-3.5-turbo",
+    })
 
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: `${prompt}` }],
-    model: "gpt-3.5-turbo",
-  });
+    return result.choices[0].message.content.toLowerCase()
+}
 
-  const response = completion.choices[0].message.content
+app.post("/chat", async (req, res) => {
+    const { prompt } = req.body
+    const ans = await findScope(prompt)
+    console.log(ans)
 
-  res.send({message:response})
+    if (ans === "no") {
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: `${prompt}` }],
+            model: "gpt-3.5-turbo",
+        })
 
+        const response = completion.choices[0].message.content
+        res.send({ message: response })
+    } else {
+        res.send({ message: "Cannot answer geography related questions." })
+    }
 })
 
-app.listen(PORT,()=>{
-    console.log(`server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
